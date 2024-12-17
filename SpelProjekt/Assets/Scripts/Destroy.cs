eeda3d58b0da669ea5eb156b4d8132557b6c2d51
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-12-17 11:13:30 +0100

    partikel brickbreaker

--- SpelProjekt/Assets/Scripts/Destroy.cs
+++ SpelProjekt/Assets/Scripts/Destroy.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    public ParticleSystem ps;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ps.Pause();
     }
 
     // Update is called once per frame
@@ -15,10 +16,13 @@ public class Destroy : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
+        
         if (other.gameObject.CompareTag("Ball"))
         {
-            
-            Destroy(this.gameObject);
+            ps.transform.parent = null;
+            ps.Play();
+            SpriteRenderer.Destroy(this.gameObject);
+            //Collider2D.Destroy(this.gameObject);
         }
     }
 }
